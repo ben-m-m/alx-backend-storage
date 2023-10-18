d8321c7c7cd8eab71c4cef8c9970c85d4b5a65a2
ben-m-m <mainaben23@gmail.com>
2023-10-18 20:45:59 +0300

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,30 +5,29 @@ import requests
 from typing import Callable
 from functools import wraps
 
-
-redis_store = redis.Redis()
-"""The module-level Redis instance.
-"""
+redis = redis.Redis()
 
 
-def wrapper(method: Callable) -> Callable:
-    """wrapper"""
-    @wraps(method)
-    def wrapped(url) -> str:
-        """wrapped"""
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
         return result
-    return wrapped
+
+    return wrapper
 
 
-@wrapper
+@wrap_requests
 def get_page(url: str) -> str:
-    """get_page"""
-    res = requests.get(url)
-    return res.text
+    """get page self descriptive
+    """
+    response = requests.get(url)
+    return response.text
