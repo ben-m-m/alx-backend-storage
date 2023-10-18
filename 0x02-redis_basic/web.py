9c18da0dc807f1044b80a86d61a3f500e2f2604f
ben-m-m <mainaben23@gmail.com>
2023-10-18 20:37:44 +0300

    Implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Implementing an expiring web cache and tracker"""
+import redis
+import requests
+from typing import Callable
+from functools import wraps
+
+
+redis_store = redis.Redis()
+"""The module-level Redis instance.
+"""
+
+
+def wrapper(method: Callable) -> Callable:
+    """wrapper"""
+    @wraps(method)
+    def wrapped(url) -> str:
+        """wrapped"""
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return wrapped
+
+
+@wrapper
+def get_page(url: str) -> str:
+    """get_page"""
+    res = requests.get(url)
+    return res.text
